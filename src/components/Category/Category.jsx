b60df161ab4ba554eb99bd25dc9a6150f419bbb6
unknown <brezanignacio@gmail.com>
2022-05-13 13:31:44 -0300

    Category styled

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,27 +1,27 @@
 import {FaPizzaSlice, FaHamburger} from 'react-icons/fa'
 import {GiNoodles, GiChopsticks} from 'react-icons/gi'
 import {NavLink} from 'react-router-dom';
-import {List} from './Category.styled'
+import {List, SLink} from './Category.styled'
 
 function Category() {
   return (
     <List>
-        <NavLink to={'/cuisine/italian'}>
+        <SLink to={'/cuisine/italian'}>
             <FaPizzaSlice/>
             <h4>Italian</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/american'}>
+        </SLink>
+        <SLink to={'/cuisine/american'}>
             <FaHamburger/>
             <h4>Amarican</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/thai'}>
+        </SLink>
+        <SLink to={'/cuisine/thai'}>
             <GiNoodles/>
             <h4>Thai</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/japanese'}>
+        </SLink>
+        <SLink to={'/cuisine/japanese'}>
             <GiChopsticks/>
             <h4>Japanese</h4>
-        </NavLink>
+        </SLink>
     </List>
   )
 }
